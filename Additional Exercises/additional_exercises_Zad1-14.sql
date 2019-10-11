9f168957ba3d8b12209328c3907582f5c13e92e8
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-10-11 09:53:16 +0300

    Update additional_exercises_Zad1-14.sql

--- Additional Exercises/additional_exercises_Zad1-14.sql
+++ Additional Exercises/additional_exercises_Zad1-14.sql
@@ -344,3 +344,188 @@ WHERE g.Id = @gameId
 ORDER BY [Item Name];
 
 GO
+
+
+--8
+USE Geography
+GO
+
+SELECT p.PeakName, 
+       m.MountainRange Mountain, 
+       p.Elevation
+FROM dbo.Mountains m
+     JOIN dbo.Peaks p ON m.Id = p.MountainId
+ORDER BY p.Elevation DESC, 
+         p.PeakName;
+
+GO
+
+
+--9
+USE Geography
+GO
+
+SELECT p.PeakName, 
+       m.MountainRange Mountain, 
+       c.CountryName, 
+       c2.ContinentName
+FROM dbo.Mountains m
+     JOIN dbo.Peaks p ON m.Id = p.MountainId
+     JOIN dbo.MountainsCountries mc ON m.Id = mc.MountainId
+     JOIN dbo.Countries c ON mc.CountryCode = c.CountryCode
+     JOIN dbo.Continents c2 ON c.ContinentCode = c2.ContinentCode
+ORDER BY p.PeakName, 
+         c.CountryName;
+
+GO
+
+--10
+USE Geography
+GO
+
+SELECT c.CountryName, 
+       c2.ContinentName, 
+       IIF(COUNT(cr.RiverId) IS NULL, 0, COUNT(cr.RiverId)) [RiversCount], 
+       IIF(SUM(r.Length) IS NULL, 0, SUM(r.Length)) [TotalLength]
+FROM dbo.Countries c
+     JOIN dbo.Continents c2 ON c.ContinentCode = c2.ContinentCode
+     LEFT JOIN dbo.CountriesRivers cr ON c.CountryCode = cr.CountryCode
+     LEFT JOIN dbo.Rivers r ON cr.RiverId = r.Id
+GROUP BY c.CountryName, 
+         c2.ContinentName
+ORDER BY [RiversCount] DESC, 
+         [TotalLength] DESC,
+		 c.CountryName;
+
+GO
+
+--11
+USE Geography
+GO
+
+SELECT c.CurrencyCode, 
+       c.Description Currency, 
+       COUNT(c2.CurrencyCode) NumberOfCountries
+FROM dbo.Currencies c
+     LEFT JOIN dbo.Countries c2 ON c.CurrencyCode = c2.CurrencyCode
+GROUP BY c.CurrencyCode, 
+         c.Description
+ORDER BY NumberOfCountries DESC, 
+         Currency;
+
+GO
+
+--12
+USE Geography
+GO
+
+SELECT c2.ContinentName, 
+       SUM(c.AreaInSqKm) CountriesArea, 
+       SUM(Convert(bigint, c.Population)) CountriesPopulation
+FROM dbo.Countries c
+     JOIN dbo.Continents c2 ON c.ContinentCode = c2.ContinentCode
+GROUP BY c2.ContinentName
+ORDER BY CountriesPopulation DESC;
+
+GO
+
+--13
+USE Geography
+GO
+
+CREATE TABLE Monasteries
+(Id          INT
+ PRIMARY KEY IDENTITY(1, 1), 
+ [Name]      NVARCHAR(MAX) NOT NULL, 
+ CountryCode CHAR(2) FOREIGN KEY REFERENCES dbo.Countries(CountryCode)
+);
+
+INSERT INTO Monasteries(Name, CountryCode) VALUES
+('Rila Monastery “St. Ivan of Rila”', 'BG'), 
+('Bachkovo Monastery “Virgin Mary”', 'BG'),
+('Troyan Monastery “Holy Mother''s Assumption”', 'BG'),
+('Kopan Monastery', 'NP'),
+('Thrangu Tashi Yangtse Monastery', 'NP'),
+('Shechen Tennyi Dargyeling Monastery', 'NP'),
+('Benchen Monastery', 'NP'),
+('Southern Shaolin Monastery', 'CN'),
+('Dabei Monastery', 'CN'),
+('Wa Sau Toi', 'CN'),
+('Lhunshigyia Monastery', 'CN'),
+('Rakya Monastery', 'CN'),
+('Monasteries of Meteora', 'GR'),
+('The Holy Monastery of Stavronikita', 'GR'),
+('Taung Kalat Monastery', 'MM'),
+('Pa-Auk Forest Monastery', 'MM'),
+('Taktsang Palphug Monastery', 'BT'),
+('Sümela Monastery', 'TR');
+
+ALTER TABLE dbo.Countries
+ADD IsDeleted BIT DEFAULT 0
+
+UPDATE Countries
+SET IsDeleted = 0;
+
+UPDATE dbo.Countries
+  SET 
+      IsDeleted = 1
+WHERE CountryCode IN
+(
+    SELECT c.CountryCode
+    FROM Countries AS c
+         JOIN CountriesRivers AS cr ON cr.CountryCode = c.CountryCode
+    GROUP BY c.CountryCode
+    HAVING COUNT(cr.RiverId) > 3
+);
+
+SELECT m.Name Monastery, 
+       c.CountryName Country
+FROM dbo.Monasteries m
+     JOIN dbo.Countries c ON m.CountryCode = c.CountryCode
+WHERE c.IsDeleted = 0
+ORDER BY m.Name;
+
+GO
+
+--14
+USE Geography
+GO
+
+UPDATE Countries
+  SET CountryName = 'Burma'
+WHERE CountryName = 'Myanmar';
+
+
+INSERT INTO dbo.Monasteries
+(Name, 
+ CountryCode
+)
+VALUES
+('Hanga Abbey', 
+(
+    SELECT c.CountryCode
+    FROM dbo.Countries c
+    WHERE c.CountryName = 'Tanzania'
+)
+),
+('Myin-Tin-Daik', 
+(
+    SELECT c.CountryCode
+    FROM dbo.Countries c
+    WHERE c.CountryName = 'Myanmar'
+)
+);
+
+SELECT cnt.ContinentName, 
+       cntr.CountryName, 
+       COUNT(m.Id) MonasteriesCount
+FROM Continents cnt
+     LEFT JOIN Countries cntr ON cnt.ContinentCode = cntr.ContinentCode
+     LEFT JOIN Monasteries m ON m.CountryCode = cntr.CountryCode
+WHERE cntr.IsDeleted = 0
+GROUP BY cnt.ContinentName, 
+         cntr.CountryName
+ORDER BY MonasteriesCount DESC, 
+         cntr.CountryName;
+
+GO		   
